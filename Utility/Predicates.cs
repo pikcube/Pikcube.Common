b3b6d92b5fa6c3f1982633751a53e84cbeffc942
Pikcube <michaelds13@gmail.com>
2026-04-23 16:53:11 -0400

    Added Common Predicates

---
+++ Utility/Predicates.cs
@@ -0,0 +1,12 @@
+﻿using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Runs;
+
+namespace Pikcube.Common.Utility;
+
+public static class Predicates
+{
+    public static Predicate<IRunState> IsModifierPresent<T>() where T : ModifierModel
+    {
+        return runState => runState.Modifiers.Any(m => m is T);
+    }
+}
