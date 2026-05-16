ec2ee3bbb589d530d83f92912e5ca8acaca29ee5
Pikcube <michaelds13@gmail.com>
2026-05-15 23:16:49 -0400

    Added Custom Loc Tables with the CustomLocTable Attribute

---
+++ Patches/CustomLocTablePatches.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Localization;
+using Pikcube.Common.Utility;
+using FileAccess = Godot.FileAccess;
+
+namespace Pikcube.Common.Patches;
+
+internal static class CustomLocTablePatches
+{
+    [HarmonyPatch(typeof(LocManager), "ListLocalizationFiles")]
+    public static class ListLocalizationFilesPatch
+    {
+        private static readonly string[] ExtraTables = [];
+
+        public static IEnumerable<string> Postfix(IEnumerable<string> __result)
+        {
+            return CustomLocManager.GetCustomLocTables(__result);
+        }
+    }
+
+    [HarmonyPatch(typeof(LocManager), "LoadTable")]
+    public static class LoadTablePatch
+    {
+        public static bool Prefix(string path, ref Dictionary<string, string> __result)
+        {
+            if (FileAccess.FileExists(path))
+            {
+                return true;
+            }
+
+            __result = [];
+            return false;
+        }
+    }
+}
---
+++ Utility/CustomLocTableAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Pikcube.Common.Utility;
+
+/// <summary>
+/// Defines a custom loc table used in this project.
+/// </summary>
+[AttributeUsage(AttributeTargets.All)]
+public class CustomLocTableAttribute : Attribute
+{
+    /// <summary>
+    /// Create a custom loc table entry.
+    /// </summary>
+    /// <param name="name">The name of the json file to register.</param>
+    public CustomLocTableAttribute(string name)
+    {
+        CustomLocManager.Register(name);
+    }
+}
