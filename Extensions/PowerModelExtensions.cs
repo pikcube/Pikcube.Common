27bf3d966c303a9433fdb090a07b5c6108d78d96
Pikcube <michaelds13@gmail.com>
2026-06-03 23:25:24 -0400

    0.2.2

---
+++ Extensions/PowerModelExtensions.cs
@@ -0,0 +1,46 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace Pikcube.Common.Extensions;
+
+/// <summary>
+/// Extension on all types that Implement PowerModel
+/// </summary>
+public static class PowerModelExtensions
+{
+    extension<T>(T instance) where T : PowerModel
+    {
+        /// <summary>
+        /// Apply a power asyncronously using PowerCmd.Apply.
+        /// </summary>
+        /// <param name="choiceContext">The current choice context</param>
+        /// <param name="target">The target creature</param>
+        /// <param name="amount">The amount to apply</param>
+        /// <param name="applier">The creature that applied this power (if any)</param>
+        /// <param name="cardSource">The card that applied this power (if any)</param>
+        /// <param name="silent">When true, don't show the power being applied</param>
+        /// <returns>The power that was just applied.</returns>
+        public static Task<T?> ApplyAsync(PlayerChoiceContext choiceContext, Creature target, decimal amount, Creature? applier, CardModel? cardSource, bool silent = false)
+        {
+            return PowerCmd.Apply<T>(choiceContext, target, amount, applier, cardSource, silent);
+        }
+
+        /// <summary>
+        /// Apply a power asyncronously using PowerCmd.Apply.
+        /// </summary>
+        /// <param name="choiceContext">The current choice context</param>
+        /// <param name="targets">The target creatures</param>
+        /// <param name="amount">The amount to apply</param>
+        /// <param name="applier">The creature that applied this power (if any)</param>
+        /// <param name="cardSource">The card that applied this power (if any)</param>
+        /// <param name="silent">When true, don't show the power being applied</param>
+        /// <returns>The power that was just applied.</returns>
+        public static Task<IReadOnlyList<T>> ApplyAsync(PlayerChoiceContext choiceContext, IEnumerable<Creature> targets, decimal amount, Creature? applier, CardModel? cardSource, bool silent = false)
+        {
+            return PowerCmd.Apply<T>(choiceContext, targets, amount, applier, cardSource, silent);
+        }
+
+    }
+}
